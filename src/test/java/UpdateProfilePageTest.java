6ea5d87c7de7f600e11ee316cad428a49ce66091
venkatsai494 <venkatsai494@gmail.com>
2026-05-11 08:33:48 +0530

    Launching the profile url in naukri

--- src/test/java/UpdateProfilePageTest.java
+++ src/test/java/UpdateProfilePageTest.java
@@ -1,2 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class UpdateProfilePageTest {
+    @Test
+    public void testUpdateProfilePage() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.naukri.com/mnjuser/profile?id=&altresid");
+    }
 }
