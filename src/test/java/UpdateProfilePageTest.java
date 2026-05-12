98193e031efefb39f21110f1ebd73503a4bcc600
venkatsai494 <venkatsai494@gmail.com>
2026-05-12 10:45:38 +0530

    Logging into the profile

--- src/test/java/UpdateProfilePageTest.java
+++ src/test/java/UpdateProfilePageTest.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -7,5 +8,8 @@ public class UpdateProfilePageTest {
     public void testUpdateProfilePage() {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.naukri.com/mnjuser/profile?id=&altresid");
+        driver.findElement(By.id("usernameField")).sendKeys("venkatsai494@gmail.com");
+        driver.findElement(By.id("passwordField")).sendKeys("");
+        driver.findElement(By.xpath("//button[text()='Login']")).click();
     }
 }
