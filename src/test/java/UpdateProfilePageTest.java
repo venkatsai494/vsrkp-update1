2834d257e0dd40df4b81b35d49f80969d5472e83
venkatsai494 <venkatsai494@gmail.com>
2026-05-15 09:18:09 +0530

    Clicking on the Save button

--- src/test/java/UpdateProfilePageTest.java
+++ src/test/java/UpdateProfilePageTest.java
@@ -5,11 +5,19 @@ import org.testng.annotations.Test;
 
 public class UpdateProfilePageTest {
     @Test
-    public void testUpdateProfilePage() {
+    public void testUpdateProfilePage() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.naukri.com/mnjuser/profile?id=&altresid");
+        driver.manage().window().maximize();
         driver.findElement(By.id("usernameField")).sendKeys("venkatsai494@gmail.com");
         driver.findElement(By.id("passwordField")).sendKeys("");
         driver.findElement(By.xpath("//button[text()='Login']")).click();
+        Thread.sleep(3000);
+        driver.navigate().to("https://www.naukri.com/mnjuser/profile?id=&altresid");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//em[contains(@class,'edit')]")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.id("saveBasicDetailsBtn")).click();
+
     }
 }
